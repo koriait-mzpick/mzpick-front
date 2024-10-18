5e96df57ad2340d601676b18ad8aa29e0fb674c3
ldh1010 <ldh5417@gmail.com>
2024-10-18 17:04:17 +0900

    0000

--- mzpick-front/src/layouts/SideBarLayout/index.tsx
+++ mzpick-front/src/layouts/SideBarLayout/index.tsx
@@ -25,7 +25,7 @@ export default function SideBarMain() {
                         <div className='Travel-detail-text'>여행 게시판</div>
                     </div>
                     <div className='Travel-detail-text-box'>
-                        <div className='icon'>2</div>
+                        <div className='icon'>3git </div>
                         <div className='Travel-detail-text'>맛집</div>
                     </div>
                     <div className='Travel-detail-text-box'>
@@ -50,36 +50,6 @@ export default function SideBarMain() {
                     <div className='slice-line'>/</div>
                     <div className='signup-button'>회원가입</div>
                 </div>
-            </div>
-        </div>
-    );
-}import { useState } from 'react';
-import './style.css';
-
-export default function SideBarMain() {
-
-    const Header = () => {
-
-        const [isOpen, setMenu] = useState(false);  // 메뉴의 초기값을 false로 설정
-
-        const toggleMenu = () => {
-            setMenu(isOpen => !isOpen); // on,off 개념 boolean
-        }
-    }
-
-    // render: 우측 네이게이션 컴포넌트//
-    return (
-        <div id='main'>
-            <div className="side-bar">
-                <div className="closeButtoon">X</div>
-                <div className="HOME">HOME</div>
-                <ul className="TRAVEL">TRAVEL
-                </ul>
-                <div className="FOOD">FOOD</div>
-                <div className="FASHION">FASHION</div>
-                <div className="KEYWORD">KEYWORD</div>
-                <div className="HALL OF FAME">HALL OF FAME</div>
-                <div className="signIn-signUp">로그인/회원가입</div>
             </div>
         </div>
     );
