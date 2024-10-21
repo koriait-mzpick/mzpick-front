2abcd6519ba09ad3e0b79f41a8f259d3a6cc4a67
jihye <jjihye0609@naver.com>
2024-10-21 15:21:39 +0900

    20241021

--- mzpick-front/src/views/Auth/index.tsx
+++ mzpick-front/src/views/Auth/index.tsx
@@ -1,12 +1,7 @@
 import React from 'react'
-import './style.css';
 
 export default function Auth() {
   return (
-    <div className="sns-container">
-    <div className="title">SNS</div>
-    <div className="sns-button-container">
-    </div>
-</div>
+    <div>index</div>
   )
 }
