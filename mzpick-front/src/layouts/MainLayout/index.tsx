1dcc725a73fc23188468ae8f131979212402a23a
ldh1010 <ldh5417@gmail.com>
2024-10-21 15:25:37 +0900

    for pull

--- mzpick-front/src/layouts/MainLayout/index.tsx
+++ mzpick-front/src/layouts/MainLayout/index.tsx
@@ -1,13 +1,21 @@
 import './style.css';
 import { useState } from 'react';
 
+// component: 메인레이아웃 컴포넌트 //
 export default function MainLayout() {
 
-  // component: 메인레이아웃 컴포넌트 //
+  // state: 사이드메뉴 상태 //
   const [sideBarOpen, setSideBarOpen] = useState(false);  // 메뉴의 초기값을 false로 설정
 
   const toggleMenu = () => {
     setSideBarOpen(!sideBarOpen); // on,off 개념 boolean
+  }
+
+  // state: 세부 카테고리 상태 //
+  const [categoryOpen, setCategoryOpen] = useState(false);  // 메뉴의 초기값을 false로 설정
+
+  const categoryMenu = () => {
+    setCategoryOpen(!categoryOpen); // on,off 개념 boolean
   }
 
   // render: 메인레이아웃 컴포넌트 렌더링 //
@@ -21,67 +29,68 @@ export default function MainLayout() {
         <div className='navi-box'>
           <div className="signin-signup">
             <div className='login-button'>로그인</div>
-            <div className='slice-line' style={{cursor:"default"}}>/</div>
+            <div className='slice-line' style={{ cursor: "default" }}>/</div>
             <div className='signup-button'>회원가입</div>
-              <div className='navi-icon' onClick={toggleMenu}></div>
+            <div className='navi-icon' onClick={toggleMenu}></div>
             {!sideBarOpen &&
-            <div>
-            <div className='main'>
-              <div className="side-bar">
-                  <div className="close-icon" onClick={toggleMenu}></div>
-                <div className={`sideBar ${sideBarOpen ? 'open' : 'closed'}`}>
-
-                  <div className="Category ">HOME</div>
-                  <div className="Category" style={{ borderBottom: "1px solid rgba(201, 224, 253, 0.3)" }}>TRAVEL</div>
-                  <div className='Category-detail'>
-                    <div className='Category-detail-text-box'>
-                      <div className='icon'></div>
-                      <div className='Category-detail-text'>여행 게시판</div>
+              <div>
+                <div className='main'>
+                  <div className="side-bar">
+                    <div className="close-icon" onClick={toggleMenu}></div>
+                    <div className="Category ">HOME</div>
+                      <div className="Category" style={{ borderBottom: "1px solid rgba(201, 224, 253, 0.3)" }} onClick={categoryMenu}>TRAVEL</div>
+                      {!categoryOpen &&
+                      <div>
+                      <div className='Category-detail'>
+                        <div className='Category-detail-text-box'>
+                          <div className='icon'></div>
+                          <div className='Category-detail-text'>여행 게시판</div>
+                        </div>
+                        <div className='Category-detail-text-box'>
+                          <div className='icon'></div>
+                          <div className='Category-detail-text'>맛집</div>
+                        </div>
+                        <div className='Category-detail-text-box'>
+                          <div className='icon'></div>
+                          <div className='Category-detail-text'>카페</div>
+                        </div>
+                        <div className='Category-detail-text-box'>
+                          <div className='icon'></div>
+                          <div className='Category-detail-text'>숙박</div>
+                        </div>
+                        <div className='Category-detail-text-box'>
+                          <div className='icon'></div>
+                          <div className='Category-detail-text'>투표</div>
+                        </div>
+                      </div>
+                      </div>
+                      }
+                    <div className="Category" style={{ borderBottom: "1px solid rgba(201, 224, 253, 0.3)" }}>FOOD</div>
+                    <div className="Category">FASHION</div>
+                    <div className='Category-detail'>
+                      <div className='Category-detail-text-box'>
+                        <div className='icon'></div>
+                        <div className='Category-detail-text'>여행 게시판</div>
+                      </div>
+                      <div className='Category-detail-text-box'>
+                        <div className='icon'></div>
+                        <div className='Category-detail-text'>맛집</div>
+                      </div>
                     </div>
-                    <div className='Category-detail-text-box'>
-                      <div className='icon'></div>
-                      <div className='Category-detail-text'>맛집</div>
+                    <div className="Category" style={{ borderBottom: "1px solid rgba(201, 224, 253, 0.3)" }}>KEYWORD</div>
+                    <div className="Category">HALL OF FAME</div>
+                    <div className="signin-signup">
+                      <div className='login-button'>로그인</div>
+                      <div className='slice-line' style={{ cursor: "default" }}>/</div>
+                      <div className='signup-button'>회원가입</div>
                     </div>
-                    <div className='Category-detail-text-box'>
-                      <div className='icon'></div>
-                      <div className='Category-detail-text'>카페</div>
-                    </div>
-                    <div className='Category-detail-text-box'>
-                      <div className='icon'></div>
-                      <div className='Category-detail-text'>숙박</div>
-                    </div>
-                    <div className='Category-detail-text-box'>
-                      <div className='icon'></div>
-                      <div className='Category-detail-text'>투표</div>
-                    </div>
-                  </div>
-                  <div className="Category" style={{ borderBottom: "1px solid rgba(201, 224, 253, 0.3)" }}>FOOD</div>
-                  <div className="Category">FASHION</div>
-                  <div className='Category-detail'>
-                    <div className='Category-detail-text-box'>
-                      <div className='icon'></div>
-                      <div className='Category-detail-text'>여행 게시판</div>
-                    </div>
-                    <div className='Category-detail-text-box'>
-                      <div className='icon'></div>
-                      <div className='Category-detail-text'>맛집</div>
-                    </div>
-                  </div>
-                  <div className="Category" style={{ borderBottom: "1px solid rgba(201, 224, 253, 0.3)" }}>KEYWORD</div>
-                  <div className="Category">HALL OF FAME</div>
-                  <div className="signin-signup">
-                    <div className='login-button'>로그인</div>
-                    <div className='slice-line'style={{cursor:"default"}}>/</div>
-                    <div className='signup-button'>회원가입</div>
                   </div>
                 </div>
               </div>
-            </div>
-            </div>
             }
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
