8a30e0f114ac62306ecae61e571c44270cb244d1
jihye <jjihye0609@naver.com>
2024-10-22 14:41:01 +0900

    MyPage Folder 작업

--- mzpick-front/src/MzPick.tsx
+++ mzpick-front/src/MzPick.tsx
@@ -1,20 +1,25 @@
 
 import { Route, Routes } from 'react-router-dom';
-import './MzPick.css';
+
 import { HOME_PATH, SIGN_IN_PATH, TRAVEL_PATH } from './constants';
 import MainLayout from './layouts/MainLayout';
-import Auth from './views/Auth';
-
-import Home from './views/Home';
 import Travel from './views/Travel';
+import MyPage from './views/MyPage';
+import Auth from './views/Auth';
+import Home from './views/Home';
+import './MzPick.css';
+import BottomNav from './layouts/BottomNav';
+
 
 export default function MzPick() {
   return (
     <Routes>
-      <Route index element={<MainLayout/>}/>
-      <Route path={HOME_PATH} element={<Home/>}/>
-      <Route path={SIGN_IN_PATH} element={<Auth/>}/>
-      <Route path={TRAVEL_PATH} element={<Travel />} />
+      <Route index element={<MyPage/>}/>
+      {/* <Route index element={<BottomNav/>}/> */}
+      {/* <Route index element={<MainLayout/>}/> */}
+      {/* <Route path={HOME_PATH} element={<Home/>}/> */}
+      {/* <Route path={SIGN_IN_PATH} element={<Auth/>}/> */}
+      {/* <Route path={TRAVEL_PATH} element={<Travel />} /> */}
     </Routes>
   );
 }
