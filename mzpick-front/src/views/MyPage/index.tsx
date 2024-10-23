2c40c9ce3522a1c8d12909782e80dc781e9d2141
jihye <jjihye0609@naver.com>
2024-10-23 14:15:33 +0900

    mypage update

--- mzpick-front/src/views/MyPage/index.tsx
+++ mzpick-front/src/views/MyPage/index.tsx
@@ -1,26 +1,39 @@
 import React from 'react'
 import './style.css';
+import { url } from 'inspector';
 
 export default function MyPage() {
   return (
-    <div>
+    <div id='componentBox'>
 
-    <div id='header-box'>
+    <div className='header-box'>
       <div className='header-namebox'>MyPage</div>
 
       <div className='user-infoBox'>
         <div className='user-contentBox'>
-          <div className='contentBox1'>제니</div>
-          <div className='contentBox1'>010-1111-1111</div>
+          <div className='contentBox1'>
+            <div className='iconBox'></div>
+            <div className='content-board'>제니</div>
+          </div>
+          <div className='contentBox1'>
+          <div className='iconBox'></div>
+          <div className='content-board'>010-1111-1111</div>
+          </div>
         </div>
         <div className='user-contentBox'>
-          <div className='contentBox2'>jenny001</div>
-          <div className='contentBox2'>Naver</div>
+          <div className='contentBox2'>
+          <div className='iconBox'></div>
+          <div className='content-board'>jenny01</div>
+          </div>
+          <div className='contentBox2'>
+          <div className='iconBox'></div>
+          <div className='content-board'>NAVER</div>
+          </div>
         </div>
       </div>
       </div>
 
-    <div id='subscribe-box'>
+    <div className='subscribe-box'>
       <div className='save-box'>
         <div className='textBox'  style={{ borderBottom: "4px solid rgba(0 , 0, 0, 100)" }} >SAVE</div>
         <div className='imageBox'>
