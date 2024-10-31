ead773c6550aaedf7ac56e327169739701364b2c
ha-myeongyun <97523237+devyunie@users.noreply.github.com>
2024-10-31 10:36:54 +0900

    지도

--- mzpick-front/src/constants/index.ts
+++ mzpick-front/src/constants/index.ts
@@ -9,7 +9,6 @@ export const ID_CHECK_PATH = '/id-check'
 export const TEL_AUTH_PATH = '/tel-auth'
 export const TEL_AUTH_CHECK_PATH = '/tel-auth-check'
 export const SIGN_UP_PATH = '/sign-up'
-
 export const TRAVEL_PATH='/travel'
 export const TRAVEL_DETAIL_PATH='/travel/detail'
 
--- mzpick-front/src/views/Travel/index.tsx
+++ mzpick-front/src/views/Travel/index.tsx
@@ -1,7 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
-export default function Travel() {
-  return (
-    <div>Travel</div>
-  )
+import './style.css';
+declare global {
+  interface Window {
+    kakao: any;
+  }
 }
+
+
+
+const MapBox = () => {
+
+  useEffect(() => {
+    let container = document.getElementById(`map`); // 지도를 담을 영역의 DOM 레퍼런스
+    let options = {
+      center: new window.kakao.maps.LatLng(37.564214, 127.001699), // 지도 중심 좌표
+      level: 11, // 지도의 레벨(확대, 축소 정도)
+    };
+
+    let map = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+  }, []);
+
+  return (
+    <div id="map" />
+  );
+};
+
+export default function TraveMap() {
+
+  return (
+    <div id='main-wrapper'>
+      <div id='map-main'>
+        <div className='right-map-wrapper'>
+          <MapBox />
+        </div>
+        <div className='bar-box'>
+          <div className='vertical-bar'></div>
+        </div>
+        <div className='left-map-wrapper'>
+          <div className='right-box'>
+            <div className='head-travel-title'>경상남도 김해시</div>
+            <div className='middle-icon-box'>
+              <div className='left-icon' />
+              <div className='middle-icon' />
+              <div className='right-icon' />
+            </div>
+            <div className='bottom-location-select'>
+              <div className='location-input-box'>
+                <input className='input-box' type="text" placeholder='검색어를 입력해주세요' />
+              </div>
+              <div className='location-selet-box'>
+                <div className='location-select'>
+                  <select className='select-box' name="" id="">
+                    <option value="">제주</option>
+                    <option value="">제주</option>
+                    <option value="">제주</option>
+                    <option value="">제주</option>
+                  </select>
+                </div>
+                <div className='location-select'>
+                  <select className='select-box' name="" id="">
+                    <option value="">제주</option>
+                    <option value="">제주</option>
+                    <option value="">제주</option>
+                    <option value="">제주</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+};
