d203c00e73ef05e2e7dd59fccc70bc76e6699e4d
ha-myeongyun <97523237+devyunie@users.noreply.github.com>
2024-11-01 14:49:45 +0900

    사진 업로드

--- mzpick-front/src/apis/travel/dto/request/post-travel.request.dto.ts
+++ mzpick-front/src/apis/travel/dto/request/post-travel.request.dto.ts
@@ -1,7 +1,7 @@
 // interface: 여행 게시글 작성 Request Body Dto //
 export default interface PostTravelRequestDto {
     travelTitle: string;
-    travelHashtagContentList: string;
+    travelHashtagContentList: string[];
     travelLocation: string;
     travelPhotoList: string[];
     travelContent: string;
