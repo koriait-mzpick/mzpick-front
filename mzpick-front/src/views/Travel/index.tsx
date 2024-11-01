0b761d640f4f40d2d3b55c32a12316b06f9eb148
ha-myeongyun <97523237+devyunie@users.noreply.github.com>
2024-11-01 10:43:03 +0900

    1234

--- mzpick-front/src/views/Travel/index.tsx
+++ mzpick-front/src/views/Travel/index.tsx
@@ -4,6 +4,10 @@ import { area } from './area';
 import './style.css';
 import { useSearchLocationStore } from 'src/stores';
 import { Map } from "react-kakao-maps-sdk"
+import { useLocation, useNavigate } from 'react-router';
+import { TRAVEL_CAFE_PATH, TRAVEL_PATH, TRAVEL_RESTAURANT_PATH, TRAVEL_STAY_PATH, VOTE_PATH } from '../../constants';
+
+
 declare global {
   interface Window {
     kakao: any;
@@ -20,6 +24,8 @@ const AreaSelect = () => {
 
   const { setSearchLocation } = useSearchLocationStore();
 
+  const [searchWord, setSearchWord] = useState<string>('');
+
 
   // 선택된 지역에 따라 하위 지역 목록을 가져옴
   const subAreas = area.find((area) => area.name === selectedArea)?.subArea || [];
@@ -28,65 +34,82 @@ const AreaSelect = () => {
   const onSelectAreaChangeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedArea(e.target.value);
     setSelectedSubArea(""); // 새로운 지역 선택 시 하위 지역 초기화
-    setSearchLocation("");
+    setSearchWord('');
+    setSearchLocation('');
   };
 
   const onSelectSubAreaChangeHanler = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedSubArea(e.target.value);
   };
 
+  const onSearchLocationChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setSearchWord(value);
+    setSelectedArea('');
+    setSearchLocation('');
+  }
+
+  const onSearchWordButtonClickHandler = () => {
+    setSearchLocation(searchWord);
+  }
+
+
   const onSearchButtonClickHandler = () => {
-    setSearchLocation(selectedArea + ' ' + selectedSubArea);
+    setSearchLocation(searchWord);
+    if (!searchWord) setSearchLocation(selectedArea + ' ' + selectedSubArea);
   }
 
   return (
     <div className='search-wrapper'>
-        <select value={selectedArea} onChange={onSelectAreaChangeHandler} className="location-select">
-          <option value="">지역을 선택해주세요</option>
-          {area.map((area) => (
-            <option key={area.name} value={area.name}>
-              {area.name}
-            </option>
-          ))}
-        </select>
-        {selectedArea && (
-          <select value={selectedSubArea} onChange={onSelectSubAreaChangeHanler} className="location-select">
-            <option value="" >시, 군, 구를 선택해주세요</option>
-            {subAreas.map((subArea) => (
-              <option key={subArea} value={subArea}>
-                {subArea}
+      <div className='search-box'>
+        <input className='input-box' type="text" value={searchWord} placeholder='검색어를 입력해주세요' onChange={onSearchLocationChangeHandler} />
+        <div className='select-box'>
+          <select value={selectedArea} onChange={onSelectAreaChangeHandler} className="location-select">
+            <option value="">지역을 선택해주세요</option>
+            {area.map((area) => (
+              <option key={area.name} value={area.name}>
+                {area.name}
               </option>
             ))}
           </select>
-        )}
+          {selectedArea && (
+            <select value={selectedSubArea} onChange={onSelectSubAreaChangeHanler} className="location-select">
+              <option value="" >시, 군, 구를 선택해주세요</option>
+              {subAreas.map((subArea) => (
+                <option key={subArea} value={subArea}>
+                  {subArea}
+                </option>
+              ))}
+            </select>
+          )}
+        </div>
+      </div>
       <div className='search-button' onClick={onSearchButtonClickHandler}>검색</div>
-    </div>
+    </div >
   );
 };
 
-const {kakao} = window;
+const { kakao } = window;
 
-const MapBox = ({searchLocation}:any) => {
+const MapBox = ({ searchLocation }: any) => {
 
-  const [position, setPosition] = useState<{lat: number, lng: number}>({lat: 37.5642135, lng: 127.269311});
-  
-  useEffect(()=>{
-    if(!searchLocation) return;
+  const [position, setPosition] = useState<{ lat: number, lng: number }>({ lat: 37.5642135, lng: 126.9769 });
+
+  useEffect(() => {
+    if (!searchLocation) return;
 
     const geocoder = new kakao.maps.services.Geocoder();
 
-    geocoder.addressSearch(searchLocation,function(result:any, status:any){
-      if(status === kakao.maps.services.Status.OK){
-        const coords = new kakao.maps.LatLng(result[0].y,result[0].x);
+    geocoder.addressSearch(searchLocation, function (result: any, status: any) {
+      if (status === kakao.maps.services.Status.OK) {
+        const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
         const position = { lng: coords.La, lat: coords.Ma };
         setPosition(position);
-      } else{
+      } else {
         alert('주소를 찾을 수 없습니다.');
       }
     })
-  },[searchLocation]);
-
-
+  }, [searchLocation]);
 
   return (
     <Map // 지도를 표시할 Container
@@ -99,23 +122,27 @@ const MapBox = ({searchLocation}:any) => {
 
 export default function TraveMap() {
 
+  const navigator = useNavigate();
+
+  const onClickMoveLocationHandler = (path: string) => {
+    navigator(path);
+  };
+
   const { searchLocation, setSearchLocation } = useSearchLocationStore();
-  const [searchWord, setSearchWord] = useState<string>('');
 
-  const onSearchLocationChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    setSearchWord(value);
-  }
+  const searchLocationUrl = useLocation();
 
-  const onSearchButtonClickHandler = () => {
-    setSearchLocation(searchWord);
-  }
+  useEffect(() => {
+    console.log(searchLocationUrl);
+    console.log(searchLocation);
+    setSearchLocation("");
+  }, [])
 
   return (
     <div id='main-wrapper'>
       <div id='map-main'>
         <div className='map-wrapper'>
-          <MapBox searchLocation={searchLocation}/>
+          <MapBox searchLocation={searchLocation} />
         </div>
         <div className='bar-box'>
           <div className='vertical-bar'></div>
@@ -124,13 +151,10 @@ export default function TraveMap() {
           <div className='right-box'>
             <div className='head-travel-title'>{searchLocation}</div>
             <div className='middle-icon-box'>
-              <div className='left-icon' />
-              <div className='middle-icon' />
-              <div className='right-icon' />
-            </div>
-            <div className='bottom-location-search'>
-              <input className='input-box' type="text" value={searchWord} placeholder='검색어를 입력해주세요' onChange={onSearchLocationChangeHandler} />
-              <div onClick={onSearchButtonClickHandler}>검색</div>
+              <div className='left-icon' onClick={() => onClickMoveLocationHandler(TRAVEL_PATH)} />
+              <div className='middle-icon-1' onClick={() => onClickMoveLocationHandler(TRAVEL_RESTAURANT_PATH)} />
+              <div className='middle-icon-2' onClick={() => onClickMoveLocationHandler(TRAVEL_CAFE_PATH)} />
+              <div className='right-icon' onClick={() => onClickMoveLocationHandler(TRAVEL_STAY_PATH)} />
             </div>
             <AreaSelect />
           </div>
