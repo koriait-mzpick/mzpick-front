766b203805fd9ade9cd2f1c62ecd1ebb5999199d
하명윤 <devyunie@hamyeong-yun-ui-MacBookPro.local>
2024-11-12 09:40:38 +0900

    1234

--- mzpick-front/src/views/Fashion/detail/index.tsx
+++ mzpick-front/src/views/Fashion/detail/index.tsx
@@ -5,7 +5,7 @@ import { useCookies } from 'react-cookie';
 import { useNavigate, useParams } from 'react-router-dom';
 import { ResponseDto } from 'src/apis/dto/response';
 import { GetTravelDetailResponseDto } from 'src/apis/travel/dto/response';
-import { ACCESS_TOKEN, FASHION_PATH } from 'src/constants';
+import { ACCESS_TOKEN, FASHION_DETAIL_PATH, FASHION_PATH, FASHION_UPDATE_PATH } from 'src/constants';
 import './style.css';
 // slider
 import Slider from "react-slick";
